1aaecccddfceaeb95f288d47e26c4be265220cb5
Sofia Rytterlund <sofia.rytterlund@gmail.com>
2018-04-16 13:04:42 +0200

    added input right and changedd eggton to cat

--- eggton/eggton.go
+++ eggton/eggton.go
@@ -1,3 +1,18 @@
+components {
+  id: "eggton"
+  component: "/eggton/eggton.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
 embedded_components {
   id: "sprite"
   type: "sprite"
@@ -32,8 +47,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: 0.0\n"
-  "      y: 58.0\n"
+  "      x: -21.0\n"
+  "      y: 129.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -48,8 +63,8 @@ embedded_components {
   "  shapes {\n"
   "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: -5.0\n"
-  "      y: -110.0\n"
+  "      x: -17.0\n"
+  "      y: -195.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -62,10 +77,10 @@ embedded_components {
   "    count: 3\n"
   "  }\n"
   "  shapes {\n"
-  "    shape_type: TYPE_SPHERE\n"
+  "    shape_type: TYPE_BOX\n"
   "    position {\n"
-  "      x: -3.0\n"
-  "      y: -17.0\n"
+  "      x: -18.0\n"
+  "      y: 5.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -75,13 +90,34 @@ embedded_components {
   "      w: 1.0\n"
   "    }\n"
   "    index: 4\n"
-  "    count: 1\n"
+  "    count: 3\n"
   "  }\n"
-  "  data: 70.6975\n"
-  "  data: 77.739\n"
+  "  shapes {\n"
+  "    shape_type: TYPE_BOX\n"
+  "    position {\n"
+  "      x: -28.0\n"
+  "      y: -115.0\n"
+  "      z: 0.0\n"
+  "    }\n"
+  "    rotation {\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
+  "      z: 0.0\n"
+  "      w: 1.0\n"
+  "    }\n"
+  "    index: 7\n"
+  "    count: 3\n"
+  "  }\n"
+  "  data: 107.1035\n"
+  "  data: 54.801\n"
   "  data: 19.7475\n"
   "  data: 11.0\n"
-  "  data: 95.1915\n"
+  "  data: 118.3995\n"
+  "  data: 64.0585\n"
+  "  data: 10.0\n"
+  "  data: 80.283\n"
+  "  data: 64.858\n"
+  "  data: 10.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
