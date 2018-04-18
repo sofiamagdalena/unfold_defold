3dbc2bfedc1c41e6e2db818879a0632fe84b993c
Sofia Rytterlund <sofia.rytterlund@gmail.com>
2018-04-18 21:27:45 +0200

    added play_animation

--- eggton/eggton.go
+++ eggton/eggton.go
@@ -17,7 +17,7 @@ embedded_components {
   id: "sprite"
   type: "sprite"
   data: "tile_set: \"/eggton/eggton.atlas\"\n"
-  "default_animation: \"eggton idle\"\n"
+  "default_animation: \"idle\"\n"
   "material: \"/builtins/materials/sprite.material\"\n"
   "blend_mode: BLEND_MODE_ALPHA\n"
   ""
