37b86ce7a5d7fe465f20d67bf6bdabcee999e939
Sofia Rytterlund <sofia.rytterlund@gmail.com>
2018-04-18 22:16:10 +0200

    added parallax background

---
+++ levels/level1/background1.go
@@ -0,0 +1,30 @@
+components {
+  id: "background1"
+  component: "/levels/level1/background1.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+components {
+  id: "bg"
+  component: "/levels/level1/bg.tilemap"
+  position {
+    x: 0.0
+    y: 0.0
+    z: -1.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- levels/level1/level1.go
+++ levels/level1/level1.go
@@ -13,21 +13,6 @@ components {
     w: 1.0
   }
 }
-components {
-  id: "bg"
-  component: "/levels/level1/bg.tilemap"
-  position {
-    x: 0.0
-    y: 0.0
-    z: -1.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
 embedded_components {
   id: "collisionobject"
   type: "collisionobject"
